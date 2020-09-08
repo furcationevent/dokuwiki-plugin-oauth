57436c59f7f8a1044953e1680d476a9538ac05a4
wintamute <wintamute@gmail.com>
2020-09-08 09:37:10 +0200

    added changes from https://github.com/cosmocode/dokuwiki-plugin-oauth/pull/82 fixes logout when using single service

--- action.php
+++ action.php
@@ -39,7 +39,12 @@ class action_plugin_oauth extends DokuWiki_Action_Plugin {
      * @return void
      */
     public function handle_start(Doku_Event &$event, $param) {
-
+        global $ID;
+        if (isset($_SESSION[DOKU_COOKIE]['oauth-logout'])){
+            unset($_SESSION[DOKU_COOKIE]['oauth-logout']);
+            send_redirect(wl($ID));
+            return;
+        }
         if (isset($_SESSION[DOKU_COOKIE]['oauth-done']['do']) || !empty($_SESSION[DOKU_COOKIE]['oauth-done']['rev'])){
             $this->restoreSessionEnvironment();
             return;
@@ -238,6 +243,12 @@ class action_plugin_oauth extends DokuWiki_Action_Plugin {
         if ($singleService == '') return true;
 
         $lang['btn_login'] = $this->getLang('loginButton') . $singleService;
+
+        if($event->data == 'logout' && $singleService != '') {
+            session_start();
+            $_SESSION[DOKU_COOKIE]['oauth-logout'] = 'logout';
+            session_write_close();
+        }
 
         if($event->data != 'login') return true;
 
